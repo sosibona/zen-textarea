f217ce7b29c3ea1b311de93e9ca502b8fb800556
sosibona <empty.tara@gmail.com>
2020-07-10 14:27:24 +0300

    solution

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,20 @@
-2
+const labelToggle = document.querySelector(".box input[type=checkbox]");
+const labelExpand = document.querySelector(".expand");
+const labelShrink = document.querySelector(".shrink");
+const wrapper = document.querySelector(".wrapper")
+const wrapperTextarea = document.querySelector(".comment-field");
+
+function changeLabel(event) {
+  wrapper.classList.toggle("wrapper-expanded");
+  wrapperTextarea.classList.toggle("textarea-expanded");
+  
+  if (event.target.checked) {
+    labelShrink.style.display = "block";
+    labelExpand.style.display = "none";
+  } else {
+    labelShrink.style.display = "none";
+    labelExpand.style.display = "block";
+  }
+}
+
+labelToggle.addEventListener('click', changeLabel)
